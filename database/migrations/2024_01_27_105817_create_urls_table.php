fb0f60d96bed5e656be3ebcf3ca7de37b22a2f39
Jahid hassan <jahid15-1499@diu.edu.bd>
2024-01-27 17:01:44 +0600

    authentication done

--- database/migrations/2024_01_27_105817_create_urls_table.php
+++ database/migrations/2024_01_27_105817_create_urls_table.php
@@ -11,11 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('urls', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->text('original_url');
+            $table->string('code')->unique();
+            $table->unsignedInteger('clicks')->default(0);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -25,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('urls');
     }
 };
